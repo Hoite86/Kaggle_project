eff647a7460650b63c9465ef17e417c840b95b08
Stephan Wilhoite <141288892+Hoite86@users.noreply.github.com>
2024-03-03 12:39:05 -0500

    first commit

---
+++ titanic_project.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import os
+
+for dirname, _, filenames in os.walk('/home/hoite/Downloads/Titanic_Project'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename)) 
+
+
+train_data = pd.read_csv('/home/hoite/Downloads/Titanic_Project/train.csv')
+train_data.head()
+test_data = pd.read_csv('/home/hoite/Downloads/Titanic_Project/test.csv')
+test_data.head()
+
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women/len(women)) * 100
+
+print("% of women who survived", rate_women)
+
+men = train_data.loc[train_data.Sex =='male']["Survived"]
+rate_men = sum(men/len(men)) * 100
+print("% of men who survived", rate_men)
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('/home/hoite/Downloads/Titanic_Project/submission.csv', index=False)
+print("Your submission was successfully saved!")
